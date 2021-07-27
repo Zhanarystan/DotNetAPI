5d2f69b92202b4fe20382f3fcddebd612e656f14
Zhanarystan <mukhtarkhanov25@gmail.com>
2021-07-27 09:09:40 +0600

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public static ICollection<string> Summaries = new List<string>
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {   
+            return Summaries;
+        }
+
+        [HttpGet("message")]
+        public IActionResult Message()
+        {
+            return Ok("Message...");
+        }
+        
+        [HttpPost]
+        public IActionResult Create(string data)
+        {
+            Summaries.Add(data);
+            return Ok(data);
+        }
+    }
+}
